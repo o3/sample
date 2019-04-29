47b0c2e25d3656d24a2ffb30834f6019c07a1380
Marat Khafizov <xafizoff@gmail.com>
2019-04-29 12:48:40 +0300

    Fix sample

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import Data.Serialize (Serialize)
 import Data.Text.Encoding (decodeUtf8)
 import Control.Concurrent.STM (atomically, readTVar)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C8
+import Debug.Trace
 
 data Example = Send | Publish BS.ByteString deriving (Show, Eq, Generic, Serialize)
 
@@ -19,15 +21,15 @@ cx = mkCx{ cxMiddleware=[router]
          }
 
 router cx@Context{cxReq=Req{reqPath=path}} =
-  let handle = case path of
-                  "/ws/samples/static/index.html" -> index
-                  "/ws/samples/static/about.html" -> about
+  let handle = case trace (C8.unpack path) path of
+                  "/ws/static/index.html" -> index
+                  "/ws/static/about.html" -> about
                   _ -> index
   in cx{cxHandler=mkHandler handle}
 
 index Init = do
   sub "room"
-  update "send" [button{id_="send", body=[literal{text = "Send"}], postback=Just Send, source=["name"]}]
+  update "send" [button{id_="send", body=[literal{text = "Send"}], postback=Just Send, source=["msg"]}]
 index (Message Send) = do
   Just x <- get "msg" -- wf:q/1
   pub "room" $ N2OClient $ Publish x
