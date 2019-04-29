9f86e214ee83bbfa64e0bb78540b1dc71fd1f17c
Marat Khafizov <xafizoff@gmail.com>
2019-04-29 08:05:46 +0300

    Add pub/sub sample

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Network.N2O
 import Network.N2O.Web hiding (Event)
 import GHC.Generics (Generic)
 import Data.Serialize (Serialize)
+import Data.Text.Encoding (decodeUtf8)
 
 data Example = Greet deriving (Show, Eq, Generic, Serialize)
 
@@ -23,11 +24,13 @@ router cx@Context{cxReq=Req{reqPath=path}} =
   in cx{cxHandler=mkHandler handle}
 
 index Init = do
-  updateText "system" "What is your name?"
+  sub "room"
+  --updateText "system" "What is your name?"
   wire button{id_="send", postback=Just Greet, source=["name"]}
 index (Message Greet) = do
-  Just name <- get "name" -- wf:q/1
-  updateText "system" ("Hello, " <> jsEscape name <> "!")
-index ev = liftIO $ putStrLn ("Unknown event" <> show ev)
+  Just x <- get "name" -- wf:q/1
+  insertBottom "system" ([panel{body = [literal{text = decodeUtf8 x}]}] :: [Element Example])
+  --updateText "system" ("Hello, " <> jsEscape x <> "!")
+index Terminate = unsub "room"
 about Init = updateText "app" "This is the N2O Hello World App"
 about ev = liftIO $ putStrLn ("Unknown event " <> show ev)
